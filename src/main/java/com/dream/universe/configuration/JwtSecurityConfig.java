97754364f237a14c59c5ddd1d2c8f04f75cd2132
SonJunHyung <kingjh1125@gmail.com>
2022-11-04 16:20:21 +0900

    memberstart

--- src/main/java/com/dream/universe/configuration/JwtSecurityConfig.java
+++ src/main/java/com/dream/universe/configuration/JwtSecurityConfig.java
@@ -1,39 +1,39 @@
-//package com.dream.universe.configuration;
-//
-//import com.dream.universe.jwt.JwtFilter;
-////import com.dream.universe.jwt.TokenProvider;
-//import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.web.DefaultSecurityFilterChain;
-//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-//
-///**
-// * <pre>
-// * Class : JwtSecurityConfig
-// * Comment: jwt 설정 클래스
-// * History
-// * ================================================================
-// * DATE             AUTHOR           NOTE
-// * ----------------------------------------------------------------
-// * 2022-10-01         이유리           최초 생성
-// * </pre>
-// *
-// * @author 이유리
-// * @version 1(클래스 버전)
-// * @see
-// */
-//
-//public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
-//
-//    private final TokenProvider tokenProvider;
-//
-//    public JwtSecurityConfig(TokenProvider tokenProvider) {
-//        this.tokenProvider = tokenProvider;
-//    }
-//
-//    @Override
-//    public void configure(HttpSecurity httpSecurity){
-//        JwtFilter customFilter = new JwtFilter(tokenProvider);
-//        httpSecurity.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
-//    }
-//}
+package com.dream.universe.configuration;
+
+import com.dream.universe.jwt.JwtFilter;
+//import com.dream.universe.jwt.TokenProvider;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+/**
+ * <pre>
+ * Class : JwtSecurityConfig
+ * Comment: jwt 설정 클래스
+ * History
+ * ================================================================
+ * DATE             AUTHOR           NOTE
+ * ----------------------------------------------------------------
+ * 2022-10-01         이유리           최초 생성
+ * </pre>
+ *
+ * @author 이유리
+ * @version 1(클래스 버전)
+ * @see
+ */
+
+public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+
+    private final TokenProvider tokenProvider;
+
+    public JwtSecurityConfig(TokenProvider tokenProvider) {
+        this.tokenProvider = tokenProvider;
+    }
+
+    @Override
+    public void configure(HttpSecurity httpSecurity){
+        JwtFilter customFilter = new JwtFilter(tokenProvider);
+        httpSecurity.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
